5706dfeb1d4a00ab7ae2490d132489941eb6473a
こう やすし <haikuoyu28@gmail.com>
2024-11-22 14:52:06 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -19,31 +19,72 @@ def connect_db():
 # 登入功能
 def login():
     conn = connect_db()
+    if conn is not None:
+        cursor = conn.cursor()
+        username = e1.get()
+        password = e2.get()
+
+        try:
+            query = "SELECT * FROM sn_id WHERE username = %s AND password = %s"
+            cursor.execute(query, (username, password))
+            result = cursor.fetchone()
+
+            if result:
+                messagebox.showinfo("登录成功", "欢迎回来！")
+            else:
+                messagebox.showerror("登录失败", "用户名或密码错误")
+        except mysql.connector.Error as err:
+            messagebox.showerror("数据库查询错误", f"发生错误：{err}")
+        finally:
+            cursor.close()
+            conn.close()
 
 
 # 注册功能
 def sign_up():
+    def register_user():
+        conn = connect_db()
+        if conn is not None:
+            cursor = conn.cursor()
+            username = e1.get()
+            password = e2.get()
+
+            try:
+                query = "INSERT INTO sn_id (username, password) VALUES (%s, %s)"
+                cursor.execute(query, (username, password))
+                conn.commit()
+                messagebox.showinfo("注册成功", "用户已注册！")
+                swindow.destroy()
+            except mysql.connector.Error as err:
+                messagebox.showerror("注册失败", f"发生错误：{err}")
+            finally:
+                cursor.close()
+                conn.close()
+
     swindow = Toplevel(root)
     swindow.title("注册界面")
     swindow.geometry("300x150")
     swindow.resizable(False, False)
 
     f3 = Frame(swindow)
-    f3.pack(pady=30)
+    f3.pack(pady=20)
     f4 = Frame(swindow)
     f4.pack()
 
     # 输入框
-    l1 = Label(f3,text="username")
-    l1.pack(side=LEFT)
-    l2 = Label(f4,text="password")
-    l2.pack(side=LEFT)
+    l3 = Label(f3,text="username")
+    l3.pack(side=LEFT)
+    l4 = Label(f4,text="password")
+    l4.pack(side=LEFT)
 
-    e1 = Entry(f3)
-    e1.pack(side=TOP)
-    e2 = Entry(f4, show="*")
-    e2.pack(side=TOP)
+    e3 = Entry(f3)
+    e3.pack(side=TOP)
+    e4 = Entry(f4, show="*")
+    e4.pack(side=TOP)
 
+    b = Button(swindow, text="注册", command=register_user,width=10)
+    b.pack(pady=10)
+    
 
 # 创建主窗口
 root = Tk()
